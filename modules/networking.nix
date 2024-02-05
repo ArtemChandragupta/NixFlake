fb4820e682fc22a475fdeb35cb1008131f23277a
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-02-06 01:22:16 +0300

    Delete modules directory
    New structureNew structure

--- modules/networking.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, config, lib, ... }:{
-
-networking.hostName = "nixos";
-networking.networkmanager.enable = true;
-
-services.logmein-hamachi.enable = true;
-programs.haguichi.enable = true;
-
-}
