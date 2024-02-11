22d1efd8aba9be9c7e8d00b90bcd346c118e8ff8
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-02-12 02:10:54 +0300

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,22 +1,16 @@
 { plgs, config, inputs, ... }:{
 
 imports = [
-  ./hardware-configuration.nix
   inputs.home-manager.nixosModules.default
-  ./modules/system.nix
-  ./modules/sound.nix
-  ./modules/locales.nix
-  ./modules/pkgs.nix
-  ./modules/usb.nix
-  ./modules/power.nix
-  ./modules/boot.nix # + kernel and autologin artem
-  ./modules/virtualisation.nix
-  ./modules/flatpak.nix # + xdg settings
-  ./modules/hyprland.nix # + display manager
-  ./modules/networking.nix
-  ./modules/user.nix
+  ./modules
   ./environment/glossynix
 ];
+
+users.users.artem = {
+  isNormalUser = true;
+  description = "artem";
+  extraGroups = [ "networkmanager" "wheel" "audio" "video" "input" "libvirtd" "docker" ];
+};
 
 home-manager = {
   extraSpecialArgs = { inherit inputs; };
