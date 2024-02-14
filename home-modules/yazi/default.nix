0af8ae6d83dea5cc13f586e9e860c577c0ab7b44
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-02-14 12:09:42 +0300

    Add files via upload

--- home-modules/yazi/default.nix
+++ home-modules/yazi/default.nix
@@ -1,22 +1,26 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, ... }:{
 
-  # yazi file manager
-  programs.yazi = {
-    enable = true;
+programs.yazi = {
+enable = true;
 
-
-    settings = {
-      manager = {
-        layout = [ 1 4 3 ];
-        sort_by = "alphabetical";
-        sort_sensitive = true;
-        sort_reverse = false;
-        sort_dir_first = true;
-        linemode = "none";
-        show_hidden = true;
-        show_symlink = true;
-      };
-    };
+settings = {
+  manager = {
+    layout = [ 1 4 3 ];
+    sort_by = "alphabetical";
+    sort_sensitive = false;
+    sort_reverse = false;
+    sort_dir_first = true;
+    linemode = "none";
+    show_hidden = false;
+    show_symlink = true;
   };
+      
+};
+};
+
+imports = [
+  ./opener.nix
+  ./binds.nix
+];
+
 }
---
+++ home-modules/yazi/opener.nix
@@ -0,0 +1,61 @@
+{ pkgs, ... }:{
+programs.yazi.settings = {
+
+  opener = {
+    "text" = [
+      {
+        exec  = "micro \"$@\"";
+        block = true;
+      }
+    ];
+    "video" = [
+      {
+        exec  = "mpv \"$@\"";
+        block = true;
+      }
+    ];
+    "image" = [
+      {
+        exec  = "imv \"$@\"";
+        block = true;
+      }
+    ];
+    "pdf" = [
+      {
+        exec  = "${pkgs.zathura}/bin/zathura \"$@\"";
+        block = true;
+      }
+    ];
+    "unarchive" = [
+      {
+        exec = ''${pkgs.unar}/bin/unar "$@"'';
+        desc = "unarchive";
+      }
+    ];
+  };
+
+  open.rules = [
+    {
+    mime = "text/*";
+    use  = "text";
+    }
+    {
+    mime = "image/*";
+    use  = "image";
+    }
+    {
+    mime = "video/*";
+    use  = "video";
+    }
+    {
+    mime = "application/pdf";
+    use  = "pdf";
+    }
+    {
+    mime = "*";
+    use  = ["text""unarchive"];
+    }
+  ];
+
+};
+}
