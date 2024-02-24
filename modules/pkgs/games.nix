4721b4ce2963d52bc39122cf6628f1537bf65d98
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-02-24 22:51:08 +0300

    Add files via upload

--- modules/pkgs/games.nix
+++ modules/pkgs/games.nix
@@ -9,4 +9,15 @@ lutris
 dosbox-x
 
 ];
+
+programs.gamemode = {
+  enable = true;
+  settings = {
+    custom = {
+      start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+      end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+    };
+  };
+};
+
 }
