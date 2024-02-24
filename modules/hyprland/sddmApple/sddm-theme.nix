05f7c5b007e9ba012dac28a0110aa55ebb866563
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-02-24 14:36:17 +0300

    Add files via upload

--- modules/hyprland/sddmApple/sddm-theme.nix
+++ modules/hyprland/sddmApple/sddm-theme.nix
@@ -3,9 +3,9 @@ pkgs.stdenv.mkDerivation {
   name = "sddm-theme";
   src = pkgs.fetchFromGitHub {
     owner = "ArtemChandragupta";
-    repo = "SDDM-theme-hello";
-    rev = "c8284cfe7d698179fd41530aa9c41d3ba5253a1d";
-    sha256 = "0aymd7mb9pp4rc755i12l2vcvh9hpn7znpy1jxwwwa5zqz4w4n30";
+    repo = "SDDM-hello";
+    rev = "fbe16986dbd2cb730a6d534c42bce50a3b5c3a64";
+    sha256 = "1bpsq31fj6hacdxbik0bq8imf435c36rv2y9314jsqpq4rjibwgr";
   };
   installPhase = ''
     mkdir -p $out
