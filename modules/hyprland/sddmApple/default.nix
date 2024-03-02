4641f41129c6ac58f919593431f1e82dc2c60ac1
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-03-03 02:34:51 +0300

    Add files via upload

--- modules/hyprland/sddmApple/default.nix
+++ modules/hyprland/sddmApple/default.nix
@@ -2,12 +2,13 @@
 
   services.xserver = {
     enable = true;
+    excludePackages = [ pkgs.xterm ];
     displayManager.sddm = {
       enable = true;
+      wayland.enable = true;
       theme = "${ import ./sddm-theme.nix { inherit pkgs; } }";
     };
   };
-
   
 environment.systemPackages = [
 pkgs.libsForQt5.qt5.qtgraphicaleffects  
