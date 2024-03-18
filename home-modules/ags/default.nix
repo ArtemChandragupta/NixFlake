298fe898016a9056ac2fbebba5c4470f35774101
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-03-19 02:01:59 +0300

    Add files via upload

---
+++ home-modules/ags/default.nix
@@ -0,0 +1,14 @@
+{ inputs, pkgs, ... }:{
+
+imports = [ inputs.ags.homeManagerModules.default ];
+
+programs.ags = {
+  enable = true;
+  configDir = ./config;
+  extraPackages = with pkgs; [
+    gtksourceview
+    webkitgtk
+    accountsservice
+  ];
+};
+}
