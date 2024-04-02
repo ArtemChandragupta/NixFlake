8acdd63db90e8e71928451c65072436f6fb51159
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-04-03 02:10:36 +0300

    Add files via upload

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,7 +1,18 @@
 { pkgs, ... }:{
 
-networking.hostName = "nixos";
-networking.networkmanager.enable = true;
+networking = {
+  hostName = "nixos";
+  networkmanager.enable = true;
+  firewall = {
+    enable = true;
+    allowedTCPPorts = [ 80 443 53317 ]; # 53317 is for LocalSend
+    allowedUDPPortRanges = [
+      { from = 4000; to = 4007; }
+      { from = 53315; to = 53318; }
+      { from = 8000; to = 8010; }
+    ];
+  };
+};
 
 environment.systemPackages = [
   pkgs.networkmanagerapplet
@@ -12,6 +23,5 @@ services.tor.enable = true;
   
 hardware.bluetooth.enable = true;
 services.blueman.enable = true;
-
 
 }
