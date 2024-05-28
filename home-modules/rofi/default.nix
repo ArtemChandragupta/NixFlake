b412d0d2ffc57cf68cac9e175e048ef14b0357b3
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-05-29 00:39:19 +0300

    Add files via upload

--- home-modules/rofi/default.nix
+++ home-modules/rofi/default.nix
@@ -1,14 +1,13 @@
 { pkgs, ... }:{
+  imports = [
+    ./appgrid.nix
+  ];
 
-imports = [
-  ./appgrid.nix
-];
+  home.packages = builtins.attrValues (import ./script.nix {inherit pkgs;});
 
-programs.rofi = {
-  enable = true;
-  package = pkgs.rofi-wayland;
-  extraConfig = {
-    terminal = "kitty";
+  programs.rofi = {
+    enable = true;
+    package = pkgs.rofi-wayland;
+    extraConfig.terminal = "kitty";
   };
-};
 }
--- home-modules/rofi/script.nix
+++ home-modules/rofi/script.nix
@@ -1,16 +1,14 @@
 { pkgs }:{
+  rofi-menu = pkgs.writeShellApplication {
+    name = "rofi-menu";
+    runtimeInputs = with pkgs; [rofi-wayland];
+    text = ''
+      dir="$HOME/.config/rofi/launchers/type-1"
+      theme='style-1'
 
-rofi-menu = pkgs.writeShellApplication {
-  name = "rofi-menu";
-  runtimeInputs = with pkgs; [rofi];
-  text = ''
-   dir="$HOME/.config/rofi/launchers/type-1"
-    theme='style-1'
-
-    rofi \
-      -show drun \
-      -theme "''${dir}"/"''${theme}".rasi
-  '';
-};
-  
+      rofi \
+        -show drun \
+        -theme "''${dir}"/"''${theme}".rasi
+    '';
+  };
 }
