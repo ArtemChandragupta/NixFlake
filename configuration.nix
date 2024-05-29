cbd4b55505fe72c45bcea701cb6f7ad97dd07c17
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-05-29 18:32:46 +0300

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,38 +1,38 @@
 { var, inputs, ... }:{
-  imports = [
-    inputs.home-manager.nixosModules.default
+
+imports = [
+  inputs.home-manager.nixosModules.default
     
-    ./modules
-    ./environment/glossynix
+  ./modules
+  ./environment/glossynix
+];
+
+users.users.${var.username} = {
+  isNormalUser = true;
+  extraGroups = [ 
+    "networkmanager"
+    "wheel"
+    "audio"
+    "video"
+    "input"
+    "libvirtd"
+    "docker"
   ];
+};
 
-  documentation.nixos.enable = false;
-
-  users.users.${var.username} = {
-    isNormalUser = true;
-    extraGroups = [ 
-      "networkmanager"
-      "wheel"
-      "audio"
-      "video"
-      "input"
-      "libvirtd"
-      "docker"
-    ];
+home-manager = {
+  extraSpecialArgs = { inherit inputs var; };
+  users = {
+    "${var.username}" = import ./home-modules/home.nix;
   };
+};
 
-  home-manager = {
-    extraSpecialArgs = { inherit inputs var; };
-    users = {
-      "${var.username}" = import ./home-modules/home.nix;
-    };
-  };
+environment.variables = {
+  TERM = "kitty";
+  TERMINAL = "kitty";
+};
 
-  environment.variables = {
-    TERM = "kitty";
-    TERMINAL = "kitty";
-  };
-
-  system.stateVersion = "23.11";
+documentation.nixos.enable = false;
+system.stateVersion = "23.11";
 
 }
--- flake.nix
+++ flake.nix
@@ -1,43 +1,43 @@
 {
-  description = "Nixos config flake";
-  
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      var = {
-        username    = "artem";
-        machine     = "ThinkBook13s";
-        environment = "glossynix";
-      };
-    in
-    {
-      nixosConfigurations = { 
-        nixos = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = {inherit inputs var;};
-          modules = [ 
-            ./configuration.nix 
-            inputs.home-manager.nixosModules.default
-          ];
-        };
-      };
-    };
-    
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+description = "Central flake";
 
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    ags.url = "github:Aylur/ags";
-    yazi.url = "github:sxyazi/yazi";
+outputs = { self, nixpkgs, home-manager, ... }@inputs:
+let
+  var = {
+    username    = "artem";
+    machine     = "ThinkBook13s";
+    environment = "glossynix";
+    new_machine = false;
   };
+in
+{
+  nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = {inherit inputs var;};
+    modules = [ 
+      ./configuration.nix 
+      inputs.home-manager.nixosModules.default
+    ];
+  };
+};
+    
+inputs = {
+  nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+
+  nixvim = {
+    url = "github:nix-community/nixvim";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+    
+  home-manager = {
+    url = "github:nix-community/home-manager";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  ags.url = "github:Aylur/ags";
+  yazi.url = "github:sxyazi/yazi";
+};
+
 }
