b7221ec7dafd8e85d053238a22f084b91182697f
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-05-29 18:39:12 +0300

    Add files via upload

--- home-modules/hyprland/hyprlock/default.nix
+++ home-modules/hyprland/hyprlock/default.nix
@@ -1,40 +1,44 @@
 { var, ... }:{
-  programs.hyprlock = {
-    enable = true;
-    settings = {
-      background = [
-        {
-          monitor = "";
-          path = "/home/${var.username}/nix/home-modules/hyprland/hyprlock/blue.png";
-        }
-      ];
 
-      input-field = [
-        {
-          monitor = "";
-          size = "300, 50";
-          outline_thickness = 0;
-          inner_color = "rgb(458588)";
-          font_color  = "rgb(282828)";
-          fade_on_empty = false;
-          placeholder_text = ''Password:'';
-          dots_spacing = 0.3;
-          dots_center = true;
-          position = "0, -440";
-        }
-      ];
+programs.hyprlock = {
 
-      label = [
-        {
-          monitor = "";
-          text = "$TIME";
-          font_size = 50;
-          color = "rgb(83a598)";
-          position = "0, 440";
-          valign = "center";
-          halign = "center";
-        }
-      ];
-    };
+  enable = true;
+  settings = {
+    background = [
+      {
+        monitor = "";
+        path = "/home/${var.username}/nix/home-modules/hyprland/hyprlock/blue.png";
+      }
+    ];
+
+    input-field = [
+      {
+        monitor = "";
+        size = "300, 50";
+        outline_thickness = 0;
+        inner_color = "rgb(458588)";
+        font_color  = "rgb(282828)";
+        fade_on_empty = false;
+        placeholder_text = ''Password:'';
+        dots_spacing = 0.3;
+        dots_center = true;
+        position = "0, -440";
+      }
+    ];
+
+    label = [
+      {
+        monitor = "";
+        text = "$TIME";
+        font_size = 50;
+        color = "rgb(83a598)";
+        position = "0, 440";
+        valign = "center";
+        halign = "center";
+      }
+    ];
   };
+  
+};
+
 }
