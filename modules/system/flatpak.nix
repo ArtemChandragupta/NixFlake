a0cf16cf501ac0cee2888d502d17443f2d283c25
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-06-07 17:55:42 +0300

    Add files via upload

--- modules/system/flatpak.nix
+++ modules/system/flatpak.nix
@@ -4,7 +4,7 @@ services.flatpak.enable = true;
 
 xdg.portal = {
   enable = true;
-  wlr.enable = true;
+  #wlr.enable = true;
   #extraPortals = [ pkgs.xdg-desktop-portal-gtk ]; - if I would delete Gnome
 };
 
