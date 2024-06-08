74a1552aa8e4f093f09589cd0f19e0dd02a18b52
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-06-08 20:33:06 +0300

    Add files via upload
    Shortened user config import

--- configuration.nix
+++ configuration.nix
@@ -22,9 +22,7 @@ users.users.${var.username} = {
 home-manager = {
   extraSpecialArgs = { inherit inputs var; };
   backupFileExtension = "hm-backup";
-  users = {
-    "${var.username}" = import ./home-modules/home.nix;
-  };
+  users."${var.username}" = import ./home-modules/home.nix;
 };
 
 environment.variables = {
