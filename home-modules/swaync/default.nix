cd122d048c0e9e40551ef5f771934b7434a0302b
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-06-10 01:54:38 +0300

    Add files via upload

--- home-modules/swaync/default.nix
+++ home-modules/swaync/default.nix
@@ -1,12 +1,45 @@
-{ pkgs, ... }:{
+{ pkgs, ... }:
 
-imports = [
-  ./config.nix
-  ./style.nix
-];
+let
+swayncConfig = {
+  "$schema" = "${pkgs.swaynotificationcenter}/etc/xdg/swaync/configSchema.json";
+  positionX = "true";
+  positionY = "top";
+  control-center-margin-top = 10;
+  control-center-margin-bottom = 10;
+  control-center-margin-right = 10;
+  control-center-margin-left = 10;
+  widgets = [
+    "dnd"
+    "mpris"
+    "buttons-grid"
+    "title"
+    "notifications"
+  ];
+  widget-config = {
+    title = {
+      text = "Notifications";
+      clear-all-buttin = true;
+      button-text = "clear";
+    };
+    dnd = {
+      text = "Do not disturb";
+    };
+    buttons-grid.actions = [
+      { label = "󰐥"; command = "systemctl poweroff"; }
+      { label = "󰜉"; command = "systemctl reboot";   }
+      { label = "󰶐"; command = "hyprlock";           }
+    ];
+  };
+}; 
+in {
 
-home.packages = with pkgs; [
-  swaynotificationcenter
-];
+home = {
+  packages = with pkgs; [ swaynotificationcenter ];
+  file = {
+    ".config/swaync/config.json".text = builtins.toJSON swayncConfig;
+    ".config/swaync/style.css".text = builtins.readFile ./style.css;
+  };
+};
 
 }
