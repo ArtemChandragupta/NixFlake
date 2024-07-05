e6fb893d7aba815a0576b2ee97d93098408d76cc
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-07-05 17:01:46 +0300

    Add files via upload

---
+++ modules/host-specific/homePC/nvidia.nix
@@ -0,0 +1,15 @@
+{ config, ...}:{
+
+services.xserver.videoDrivers = ["nvidia"];
+
+hardware.nvidia = {
+
+  modesetting.enable = true;
+  powerManagement.enable = false;
+  powerManagement.finegrained = false;
+  open = false;
+  nvidiaSettings = true;
+  package = config.boot.kernelPackages.nvidiaPackages.stable;
+};
+
+}
