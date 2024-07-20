c40a42c8cf1ae14635d8b0bc868fb8874249075b
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-07-20 03:52:22 +0300

    Add files via upload

--- home-modules/anyrun/default.nix
+++ home-modules/anyrun/default.nix
@@ -4,6 +4,13 @@ imports = [
   inputs.anyrun.homeManagerModules.default
 ];
 
+nix.settings = {
+  extra-substituters = [ "https://anyrun.cachix.org" ];
+  extra-trusted-public-keys = [
+    "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+  ];
+};
+
 programs.anyrun = {
   enable = true;
   config = {
