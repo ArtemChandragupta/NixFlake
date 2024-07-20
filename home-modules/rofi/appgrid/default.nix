d20f260fb0ea2dfd51aea66e0c9f19cd7522a82e
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-07-20 03:50:26 +0300

    Add files via upload

--- home-modules/rofi/appgrid/default.nix
+++ home-modules/rofi/appgrid/default.nix
@@ -2,7 +2,7 @@
 
 home = {
   packages = builtins.attrValues (import ./pkg.nix {inherit pkgs;});
-  file.".config/rofi/launchers/rofi-appgrid/style.rasi".text = builtins.readFile ( ./style.rasi );
+  file.".config/rofi/launchers/rofi-appgrid/style.rasi".source = ./style.rasi;
 };
 
 }
