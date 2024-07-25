d068909575b3def681aabfe8f8d688ab84c1d148
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-07-25 18:56:39 +0300

    Add files via upload

--- home-modules/hyprland/hyprlock/default.nix
+++ home-modules/hyprland/hyprlock/default.nix
@@ -4,9 +4,15 @@ programs.hyprlock = {
 
   enable = true;
   settings = {
+
+    general = {
+      hide_cursor = true;
+    };
+  
     background = [
       {
         monitor = "";
+        color = "rgb(282828)";
         path = "/home/${var.username}/nix/home-modules/hyprland/hyprlock/blue.png";
       }
     ];
@@ -18,8 +24,10 @@ programs.hyprlock = {
         outline_thickness = 0;
         inner_color = "rgb(458588)";
         font_color  = "rgb(282828)";
+        fail_color  = "rgb(cc241d)";
         fade_on_empty = false;
         placeholder_text = ''Password:'';
+        fail_text = ''Oh no...'';
         dots_spacing = 0.3;
         dots_center = true;
         position = "0, -440";
