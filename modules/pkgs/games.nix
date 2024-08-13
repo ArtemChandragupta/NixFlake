78b187bd104f280270c5083a7487f29d2d000e27
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-08-13 16:23:01 +0300

    Add files via upload

--- modules/pkgs/games.nix
+++ modules/pkgs/games.nix
@@ -13,15 +13,12 @@ environment.systemPackages = with pkgs; [
   prismlauncher
 ];
 
-programs = {
-  gamemode = {
-    enable = true;
-    settings.custom = {
-      start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-      end   = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
-    };
+programs.gamemode = {
+  enable = true;
+  settings.custom = {
+    start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+    end   = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
   };
-  steam.enable = true;
 };
 
 }
