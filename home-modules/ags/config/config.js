0202871ca0a1fe55e061a59ebf1ade28cd982089
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-08-14 22:07:18 +0300

    Add files via upload

--- home-modules/ags/config/config.js
+++ home-modules/ags/config/config.js
@@ -1,13 +1,13 @@
 import { Bar } from "./bar/main.js"
-import { applauncher } from "./applauncher/main.js"
-import { win } from "./icons/main.js"
+//import { applauncher } from "./applauncher/main.js"
+//import { win } from "./icons/main.js"
 
 App.config({
   style: "./style.css",
   windows: [
     Bar(),
-    applauncher,
-    win
+    //applauncher,
+    //win
   ],
 })
 
