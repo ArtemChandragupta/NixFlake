6487776376d18d8cbdc23a8339987ff9a30b1bf9
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-08-14 21:56:10 +0300

    Add files via upload

--- modules/host-specific/ThinkBook13s/AMD.nix
+++ modules/host-specific/ThinkBook13s/AMD.nix
@@ -2,11 +2,12 @@
 
 environment.systemPackages = with pkgs; [
   amdgpu_top
+  microcodeAmd
 ];
 
 boot = { 
-  kernelParams = ["amd_pstate=active"];
-  extraModulePackages = with config.boot.kernelPackages; [ zenpower ];
+  #kernelParams = [ "amd_pstate=active" "processor.max_cstate=1" ];
+  #extraModulePackages = with config.boot.kernelPackages; [ zenpower ];
 };
 
 services.xserver.videoDrivers = ["amdgpu"];
