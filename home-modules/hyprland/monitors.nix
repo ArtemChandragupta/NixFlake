3812b04dfabec25185c9c8a1b38540c6bda91e1d
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-08-16 15:28:16 +0300

    Add files via upload

--- home-modules/hyprland/monitors.nix
+++ home-modules/hyprland/monitors.nix
@@ -1,12 +1,14 @@
 { var, ... }:{
 
 wayland.windowManager.hyprland.settings = {
-  monitor = if var.host == "thinkBook13s" then [
+
+  monitor = if var.host == "ThinkBook13s" then [
     "eDP-1,1920x1200,0x0,1"
     "HDMI-A-1,1920x1080,0x1600,1"
   ] else if var.host == "homePC" then [
     "HDMI-A-1,1920x1080,0x0,1"
   ] else [ ];
+  
   bind = [
     "$mainMod SHIFT, up,   movecurrentworkspacetomonitor, HDMI-A-1"
     "$mainMod SHIFT, down, movecurrentworkspacetomonitor, eDP-1"
