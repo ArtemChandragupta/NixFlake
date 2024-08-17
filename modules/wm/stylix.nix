243250e2dda3efa1e4de476278de75db58a6a82e
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-08-17 12:47:54 +0300

    Add files via upload

--- modules/wm/stylix.nix
+++ modules/wm/stylix.nix
@@ -1,8 +1,11 @@
-{
+{ pkgs, ... }:{
 
 stylix = {
   autoEnable = false;
-  image = ../../home-modules/./hyprland/hyprlock/blue.png;
+  image = pkgs.fetchurl {
+    url = "https://github.com/lunik1/nixos-logo-gruvbox-wallpaper/blob/master/png/gruvbox-dark-blue.png?raw=true";
+    sha256 = "1jrmdhlcnmqkrdzylpq6kv9m3qsl317af3g66wf9lm3mz6xd6dzs";
+  };
 };
 
 }
