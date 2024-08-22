98f3306ddd45c559def31438c02ed4dd742394db
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-08-22 19:57:02 +0300

    Add files via upload

--- home-modules/firefox/default.nix
+++ home-modules/firefox/default.nix
@@ -2,9 +2,9 @@
 
 home.file.".mozilla/firefox/default/chrome".source = pkgs.fetchFromGitHub {
   owner = "KiKaraage";
-  repo = "ArcWTF";
-  rev = "885fa7d5e730e3937e3a8b7e966a36ff2421c744";
-  hash = "sha256-QUsb9XsP3VT0wFCUJGMHkF/AtgCYt5xIdvkLvqg90Xs=";
+  repo  = "ArcWTF";
+  rev   = "885fa7d5e730e3937e3a8b7e966a36ff2421c744";
+  hash  = "sha256-QUsb9XsP3VT0wFCUJGMHkF/AtgCYt5xIdvkLvqg90Xs=";
 };
 
 programs.firefox = {
@@ -14,53 +14,58 @@ programs.firefox = {
       sidebery
       userchrome-toggle-extended
     ];
-
-    bookmarks = [
-      {
-        name = "nix search";
-        url = "https://search.nixos.org";
-      }
-      {
-        name = "MyNixOS";
-        url = "https://mynixos.com";
-      }
-      {
-        name = "Github";
-        url = "https://github.com";
-      }
-    ];
-
     settings = {
-      "browser.newtabpage.pinned" = [
-        {
-          title = "Nix search";
-          url = "https://search.nixos.org";
-        }
-        {
-          title = "MyNixOS";
-          url = "https://mynixos.com";
-        }
-        {
-          title = "Github";
-          url = "https://github.com";
-        }
-      ];
+      # Performance
+      "gfx.webrender.all" = true;
+      "media.ffmpeg.vaapi.enabled"  = true;
+      "widget.dmabuf.force-enabled" = true;
 
+      # Disable useless things
       "browser.toolbars.bookmarks.visibility" = "never";
       "browser.tabs.tabmanager.enabled" = false;
       "browser.translations.enable" = false;
-
       "identity.fxaccounts.enabled" = false;
+      "browser.vpn_promo.enabled" = false;
       "extensions.pocket.enabled" = false;
-      "browser.startup.page" = 3; # Restore session
+      "extensions.htmlaboutaddons.recommendations.enabled" = false;
 
-      "extensions.autoDisableScopes" = 0; # Auto-enable extesions 
+      # Enable cool bahavior
+      "browser.startup.page" = 3; # Restore session
+      "browser.backspace_action" = 0; # Go back by Backspace
+      "extensions.autoDisableScopes" = 0; # Auto-enable extesions
+      "browser.tabs.loadBookmarksInTabs" = true;
+      "startup.homepage_welcome_url" = "about:newtab";
 
       #ArcWTF need this
       "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       "svg.context-properties.content.enabled" = true;
 
-      "startup.homepage_welcome_url" = "https://github.com/ArtemChandragupta/NixFlake";
+      "browser.newtabpage.pinned" = builtins.toJSON [
+        {
+          title = "VK";
+          url   = "https://vk.com/";
+        }
+        {
+          title = "Github";
+          url   = "https://github.com/";
+        }
+        {
+          title = "Nix search";
+          url   = "https://search.nixos.org/";
+        }
+        {
+          title = "MyNixOS";
+          url   = "https://mynixos.com/";
+        }
+        {
+          title = "Gmail";
+          url   = "https://mail.google.com/";
+        }
+        {
+          title = "Google drive";
+          url   = "https://drive.google.com/";
+        }
+      ];
     };
 
   };
