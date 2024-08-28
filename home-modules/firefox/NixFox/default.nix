441ec8f10547838a99c046e77b00708afc391e70
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-08-29 02:10:36 +0300

    Add files via upload

--- home-modules/firefox/NixFox/default.nix
+++ home-modules/firefox/NixFox/default.nix
@@ -2,10 +2,12 @@
 
 programs.firefox.profiles.default = {
   extensions = with inputs.firefox-addons.packages."x86_64-linux"; [ sidebery ];
-  userChrome  = builtins.readFile ./userChrome.css;
+  #userChrome  = builtins.readFile ./userChrome.css;
   settings = {
     "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
     "svg.context-properties.content.enabled" = true;
+
+    "ui.key.menuAccessKeyFocuses" = false; #Remove ALT menu
   };
 };
 
