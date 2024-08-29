19bed2cdb9ec32e65e36d6e646cc7723a074a3ef
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-08-30 02:57:05 +0300

    Add files via upload

--- home-modules/firefox/NixFox/default.nix
+++ home-modules/firefox/NixFox/default.nix
@@ -2,7 +2,7 @@
 
 programs.firefox.profiles.default = {
   extensions = with inputs.firefox-addons.packages."x86_64-linux"; [ sidebery ];
-  #userChrome  = builtins.readFile ./userChrome.css;
+  userChrome  = builtins.readFile ./userChrome.css;
   settings = {
     "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
     "svg.context-properties.content.enabled" = true;
