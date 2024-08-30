b2a15fa04850e01568da27cff7f9e8bed7d50c30
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-08-30 23:52:25 +0300

    Add files via upload

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,16 +1,25 @@
-{ pkgs, ... }:{
+{ pkgs, var, ... }:{
 
 networking = {
   hostName = "nixos";
   networkmanager.enable = true;
   firewall = {
-    enable = true;
+    enable = false;
     allowedTCPPorts = [ 80 443 53317 ]; #53317 for localsend
     allowedUDPPortRanges = [
       { from = 4000;  to = 4007;  }
       { from = 53315; to = 53318; }
       { from = 8000;  to = 8010;  }
     ];
+  };
+};
+
+services = {
+  syncthing = {
+    enable = true;
+    user = "${var.user}";
+    dataDir = "/home/${var.user}/Sync";
+    configDir = "/home/${var.user}/Sync/.config";
   };
 };
 
