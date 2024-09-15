868e786e46353bc9751f7c41788b842adf0f214b
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-09-15 15:50:24 +0300

    Add files via upload

--- home-modules/xdg.nix
+++ home-modules/xdg.nix
@@ -10,12 +10,22 @@ xdg = {
       "application/pdf" = [ "org.gnome.Evince.desktop" ];
       "image/*"         = [ "org.gnome.Loupe.desktop"  ];
       "video/*"         = [ "totem.desktop"            ];
+
+      "text/html" = [ "firefox.desktop" ];
+      "text/xml" =  [ "firefox.desktop" ];
+      "x-scheme-handler/http" =  [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
     };
     
     defaultApplications = {
       "application/pdf" = [ "org.gnome.Evince.desktop" ];
       "image/*"         = [ "org.gnome.Loupe.desktop"  ];
       "video/*"         = [ "totem.desktop"            ];
+
+      "text/html" = [ "firefox.desktop" ];
+      "text/xml" =  [ "firefox.desktop" ];
+      "x-scheme-handler/http" =  [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
     };
     
   };
