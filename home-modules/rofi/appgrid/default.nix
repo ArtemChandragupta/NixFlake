0ef12aa659a4528685922cc60e859a04eb2060f9
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-09-18 13:59:18 +0300

    Delete home-modules/rofi directory

--- home-modules/rofi/appgrid/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:{
-
-home = {
-  packages = builtins.attrValues (import ./pkg.nix {inherit pkgs;});
-  file.".config/rofi/launchers/rofi-appgrid/style.rasi".source = ./style.rasi;
-};
-
-}
--- home-modules/rofi/cliphist/script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -z "$1" ]; then
-    cliphist list
-else
-    cliphist decode <<<"$1" | wl-copy
-fi
