1089a6f1bcb914d56d53221a97a89bcc59e0178e
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-09-20 23:55:11 +0300

    Add files via upload

--- home-modules/languages/python.nix
+++ home-modules/languages/python.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:{
 
 home.packages = with pkgs; [
-  (python312.withPackages (ps: with ps; [
+  (python3.withPackages (ps: with ps; [
     numpy # these two are
     scipy # probably redundant to pandas
     jupyterlab
