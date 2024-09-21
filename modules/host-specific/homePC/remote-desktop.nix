730e05f9da8fd015b9f6e83b1940b20ec17e3b7b
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-09-21 13:21:34 +0300

    Add files via upload

--- modules/host-specific/homePC/remote-desktop.nix
+++ modules/host-specific/homePC/remote-desktop.nix
@@ -1,7 +1,10 @@
-{ pkgs, ... }:{
+{
 
-environment.systemPackages = with pkgs; [
-  sunshine
-];
+services.sunshine = {
+  enable = true;
+  autoStart = true;
+  capSysAdmin = true;
+  openFirewall = true;
+};
 
 }
