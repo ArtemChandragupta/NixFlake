e0869ee3ddc83ed6b8548f725472c7c202b9bd29
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-09-29 16:16:47 +0300

    Add files via upload

--- home-modules/terminal/nixvim/default.nix
+++ home-modules/terminal/nixvim/default.nix
@@ -3,20 +3,24 @@
 imports = [
   inputs.nixvim.homeManagerModules.nixvim
 
-  ./plugins.nix
+  ./languages
   ./keymap.nix
+  ./plugins.nix
 ];
 
 programs.nixvim = {
   enable = true;
   clipboard.providers.wl-copy.enable = true;
+  clipboard.register = "unnamedplus";
   luaLoader.enable = true;
   opts = {
     number = true;
     relativenumber = true;
-    cursorline = true;
+    # cursorline = true;
     shiftwidth = 2;
     scrolloff  = 5;
+    whichwrap = "<,>,[,]";
+    signcolumn = "no";
   };
   colorschemes.gruvbox.enable = true;
   extraConfigVim = /*vimsc*/''
--- home-modules/terminal/nixvim/keymap.nix
+++ home-modules/terminal/nixvim/keymap.nix
@@ -6,8 +6,15 @@ programs.nixvim = {
     maplocalleader = " ";
   };
   keymaps = [
-    { mode = [ "n" "v" ]; key = "<y>";   action = ''"+y'';      }
-    { mode = "i";         key = "<C-v>"; action = ''"+p''; }
+    { mode = [ "n" "v" ];     key = "<y>";   action = ''"+y"''; }
+    { mode = "i"; lua = true; key = "<C-v>"; action = ''
+        function ()
+          vim.api.nvim_paste(vim.fn.getreg('+'), true, -1)
+        end
+      '';
+    }
+    { mode = "n";     key = "<C-c>";   action = "gcc"; options.remap = true; }
+    { mode = "v";     key = "<C-c>";   action = "gc";  options.remap = true; }
   ];
 };
 
