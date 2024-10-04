85adcb5072e46c686aec4e506c4e1c271ddff097
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-10-04 03:56:50 +0300

    Add files via upload

--- home-modules/hyprland/rofi-appgrid/pkg.nix
+++ home-modules/hyprland/rofi-appgrid/pkg.nix
@@ -3,11 +3,7 @@
 rofi-appgrid = pkgs.writeShellApplication {
   name = "rofi-appgrid";
   runtimeInputs = with pkgs; [rofi-wayland];
-  text = ''
-    rofi \
-      -show drun \
-      -theme .config/rofi/launchers/rofi-appgrid/style.rasi
-  '';
+  text = "rofi -show drun -theme .config/rofi/launchers/rofi-appgrid/style.rasi";
 };
 
 }
