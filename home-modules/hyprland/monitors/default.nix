ecc8195c594c3d6704aa2b710272f389c916da60
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-10-11 17:59:17 +0300

    Add files via upload

--- home-modules/hyprland/monitors/default.nix
+++ home-modules/hyprland/monitors/default.nix
@@ -9,10 +9,10 @@ home.packages = [rotateMonitorScript];
 wayland.windowManager.hyprland.settings = {
 
   monitor = if var.host == "ThinkBook13s" then [
-    "eDP-1,1920x1200,0x0,1"
-    "HDMI-A-1,1920x1080,0x1600,1"
+    "eDP-1,1920x1200, 0x0, 1"
+    "HDMI-A-1,1920x1080, 0x1600, 1"
   ] else if var.host == "homePC" then [
-    "HDMI-A-1,1920x1080,0x0,1"
+    "HDMI-A-1,1920x1080@144, 0x0, 1"
   ] else [ ];
   
   bind = [
