e74aaac1d69715935f0d79f59692d017d3104cbb
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-10-11 19:44:58 +0300

    Add files via upload

--- modules/wm/ly.nix
+++ modules/wm/ly.nix
@@ -2,10 +2,10 @@
 
 services.displayManager.ly = {
   enable = true;
-  # settings = {
-  #   animation = "doom";
-  #   hide_borders = true;
-  # };
+  settings = {
+    animation = "doom";
+    hide_borders = true;
+  };
 };
 
 }
