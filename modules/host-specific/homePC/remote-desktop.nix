35946fb061a3b8add04d376858a2ec868ac911d3
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-10-20 16:50:22 +0300

    Update remote-desktop.nix

--- modules/host-specific/homePC/remote-desktop.nix
+++ modules/host-specific/homePC/remote-desktop.nix
@@ -4,14 +4,3 @@ environment.systemPackages = [
   pkgs.sunshine # Server for remote  streaming
   pkgs.stash    # No comments
 ];
-
-services.displayManager.autoLogin = { # Autologin
-  enable = true;
-  user = "artem";
-};
-systemd.services = { # Autologin workaround
-  "getty@tty1".enable = false;
-  "autovt@tty1".enable = false;
-};
-
-}
