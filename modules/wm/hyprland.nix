b67f25a95e6b94e107f2cc814e39611843a9b4a8
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-10-21 13:59:53 +0300

    Add files via upload

--- modules/wm/hyprland.nix
+++ modules/wm/hyprland.nix
@@ -1,18 +1,8 @@
-{ pkgs, var, inputs, ... }:{
+{ pkgs, ... }:{
 
 programs.hyprland = {
   enable  = true;
-  package = if var.init 
-    then pkgs.hyprland
-    else inputs.hyprland.packages.${pkgs.system}.hyprland;
   xwayland.enable = true;
-};
-
-nix.settings = {
-  substituters = [ "https://hyprland.cachix.org" ];
-  trusted-public-keys = [
-    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-  ];
 };
 
 environment.variables.EDITOR = "hx";
