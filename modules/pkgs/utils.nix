43b60ea32ef0268bc4aa30afaa71ce0fe42bc06d
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-10-23 14:55:21 +0300

    add tumbnailer

--- modules/pkgs/utils.nix
+++ modules/pkgs/utils.nix
@@ -4,6 +4,7 @@ environment.systemPackages = with pkgs; [
   brightnessctl # Brightness control
   libnotify     # Notification sender
   ffmpeg  # Lib for media manipulations
+  ffmpegthumbnailer # Lib for tumbnails
   ripdrag # Drag and drop from terminal
 
   wget   # Get things from internet
