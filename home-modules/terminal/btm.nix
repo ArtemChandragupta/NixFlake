ead7d9df46ce3d81d3e9cc0fe9ac0b904ec5283e
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-11-01 13:42:18 +0300

    Add files via upload

--- home-modules/terminal/btm.nix
+++ home-modules/terminal/btm.nix
@@ -1,10 +1,19 @@
-{ pkgs, ... }:let
-  open-btm = pkgs.writers.writeNuBin "open-resourceMonitor" 
-  "focusCursor center | kitty -o background_opacity=0.95 --class translator -e 'btm'";
+{ pkgs, ... }:
+let
+  open-btm = pkgs.writers.writeNuBin "open-resourceMonitor" /*nu*/''
+    do --ignore-errors {pkill btm}
+    focusCursor center
+    kitty -o background_opacity=0.95 --class resourceMonitor -e "btm"
+  '';
 in{
 
 programs.bottom.enable = true;
 
 home.packages = [ open-btm ];
+
+wayland.windowManager.hyprland.settings.windowrulev2 = [
+  "float        , class:(resourceMonitor)"
+  "size 1200 900, class:(resourceMonitor)"
+];
 
 }
