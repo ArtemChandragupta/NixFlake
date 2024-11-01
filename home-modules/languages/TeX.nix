f6710ea4ed8db90a671b01623f2bd976042ad8f6
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-11-01 13:41:20 +0300

    Add files via upload

--- home-modules/languages/TeX.nix
+++ home-modules/languages/TeX.nix
@@ -4,15 +4,20 @@ let
     inherit (pkgs.texlive) scheme-medium
     
     biblatex-gost biblatex biber # for bibleography
-    csquotes # quotes for references
-    wrapfig  # smthng for images
-    amsmath  # math
+    csquotes  # quotes for references
+    wrapfig   # smthng for images
+    amsmath   # math
+    multirow  # good tables
+    titlesec  # for section titles
+    tocvsec2  # for tables
+    chngcntr  # for counters
     hyperref; # links
   });
 in {
 
 home.packages = with pkgs; [
   tex # Runtime
+  texmaker
 ];
 
 }
--- home-modules/languages/haskell.nix
+++ home-modules/languages/haskell.nix
@@ -3,9 +3,8 @@
 home.packages = with pkgs.haskellPackages; [
   ghc # Compiler
   haskell-language-server # Server for helix
-  # ghcitui # Tui for interpriter (Broken)
-  # cabal-install # What is it?
-  # stack # The Haskell tool stack
+  cabal2nix
+  cabal-install
 ];
 
 }
