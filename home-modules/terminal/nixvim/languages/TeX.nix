2e9cd2c7d6a7d1a76490f3abbe733798e3f96030
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-12-04 21:17:41 +0300

    Add files via upload

--- home-modules/terminal/nixvim/languages/TeX.nix
+++ home-modules/terminal/nixvim/languages/TeX.nix
@@ -1,20 +1,7 @@
 { pkgs, ... }:{
 
-programs.nixvim.plugins = {
-  lsp.servers.texlab = {
-    enable = true;
-  };
-  vimtex = {
-    enable = true;
-    settings = {
-      compiler_method = "tectonic";
-      highlight.enable = false;
-      view_method = "zathura";
-    };
-  };
-  treesitter.grammarPackages = [
-    pkgs.vimPlugins.nvim-treesitter.builtGrammars.latex
-  ];
-};
+home.packages = [
+  pkgs.texliveFull
+];
 
 }
--- home-modules/terminal/nixvim/languages/default.nix
+++ home-modules/terminal/nixvim/languages/default.nix
@@ -1,33 +1,9 @@
-{ pkgs, ... }:{
+{ var, ... }:{
 
-imports = [
-  ./css.nix
+imports = if !var.init then [
   ./haskell.nix
-  ./js.nix
-  ./lua.nix
-  ./nushell.nix
   ./python.nix
   ./TeX.nix
-  ./toml.nix
-];
-
-programs.nixvim.plugins = {
-  lsp = {
-    enable = true;
-    servers.nil_ls.enable = true;
-  };
-  treesitter = {
-    enable = true;
-    nixGrammars = true;
-    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-      nix
-    ];
-    settings.highlight.enable = true;
-  };
-  openscad = {
-    enable = true;
-  };
-  # rainbow-delimiters.enable = true;
-};
+] else [ ];
 
 }
