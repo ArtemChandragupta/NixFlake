40ac529b8b41a109978ebbba79a14f46d182e6c4
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-12-04 18:23:21 +0300

    Add files via upload

--- home-modules/languages/TeX.nix
+++ home-modules/languages/TeX.nix
@@ -1,27 +1,7 @@
-{ pkgs, ... }:
-let
-  tex = (pkgs.texlive.combine {
-    inherit (pkgs.texlive) scheme-medium
-
-    biblatex-gost biblatex biber # for bibleography
-    eqexpl    # formula explanations
-    csquotes  # quotes for references
-    wrapfig   # smthng for images
-    amsmath   # math
-    lipsum    # random text
-    fancyhdr  # for footers
-    multirow  # good tables
-    titlesec  # for section titles
-    tocvsec2  # for tables
-    chngcntr  # for counters
-    listings  # for code blocks
-    fancyvrb  # for centered monospace element
-    hyperref; # links
-  });
-in {
+{ pkgs, ... }:{
 
 home.packages = with pkgs; [
-  tex # Runtime
+  texliveFull
   tectonic
 ];
 
