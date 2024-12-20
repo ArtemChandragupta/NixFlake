07761eaa1b8b332a3b5aa2b1b9bba34fae32a551
ArtemChandragupta <90765302+ArtemChandragupta@users.noreply.github.com>
2024-12-20 18:42:36 +0300

    Add files via upload

--- modules/wm/dm.nix
+++ modules/wm/dm.nix
@@ -1,17 +1,8 @@
-{ var, ... }:{
+{
 
 services.xserver.displayManager.gdm = {
-  # enable  = if var.host == "homePC" then true else false;
   enable  = true;
   wayland = true;
 };
-
-# services.displayManager.ly = {
-#   enable = if var.host == "ThinkBook13s" then true else false;
-#   settings = {
-#     animation = "doom";
-#     hide_borders = true;
-#   };
-# };
 
 }
