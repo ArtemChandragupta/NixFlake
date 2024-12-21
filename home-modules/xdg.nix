122cc3ab418ba6746106015a06f5e85a45d5be33
ArtemChandragupta <artem020503@gmail.com>
2024-12-21 21:24:37 +0300

    Change mime

--- home-modules/xdg.nix
+++ home-modules/xdg.nix
@@ -2,29 +2,17 @@
 
 xdg = {
   enable = true;
-  mime.enable = true;
+  # mime.enable = true;
   mimeApps = {
     enable = true;
-    
-    associations.added = {
-      "application/pdf" = [ "org.gnome.Evince.desktop" ];
-      "image/*"         = [ "org.gnome.Loupe.desktop"  ];
-      "video/*"         = [ "totem.desktop"            ];
-
-      "text/html" = [ "firefox.desktop" ];
-      "text/xml" =  [ "firefox.desktop" ];
-      "x-scheme-handler/http" =  [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
-    };
-    
     defaultApplications = {
-      "application/pdf" = [ "org.gnome.Evince.desktop" ];
+      "application/pdf" = [ "org.gnome.Papers.desktop" ];
       "image/*"         = [ "org.gnome.Loupe.desktop"  ];
       "video/*"         = [ "totem.desktop"            ];
 
-      "text/html" = [ "firefox.desktop" ];
-      "text/xml" =  [ "firefox.desktop" ];
-      "x-scheme-handler/http" =  [ "firefox.desktop" ];
+      "text/html"              = [ "firefox.desktop" ];
+      "text/xml"               = [ "firefox.desktop" ];
+      "x-scheme-handler/http"  = [ "firefox.desktop" ];
       "x-scheme-handler/https" = [ "firefox.desktop" ];
     };
     
