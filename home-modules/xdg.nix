a6e279966f370cf3f9243b1a3542e616786fff84
ArtemChandragupta <artem020503@gmail.com>
2024-12-21 22:04:45 +0300

    mime without []

--- home-modules/xdg.nix
+++ home-modules/xdg.nix
@@ -5,17 +5,26 @@ xdg = {
   # mime.enable = true;
   mimeApps = {
     enable = true;
+    # defaultApplications = {
+    #   "application/pdf" = [ "org.gnome.Papers.desktop" ];
+    #   "image/*"         = [ "org.gnome.Loupe.desktop"  ];
+    #   "video/*"         = [ "totem.desktop"            ];
+    #
+    #   "text/html"              = [ "firefox.desktop" ];
+    #   "text/xml"               = [ "firefox.desktop" ];
+    #   "x-scheme-handler/http"  = [ "firefox.desktop" ];
+    #   "x-scheme-handler/https" = [ "firefox.desktop" ];
+    # };
     defaultApplications = {
-      "application/pdf" = [ "org.gnome.Papers.desktop" ];
-      "image/*"         = [ "org.gnome.Loupe.desktop"  ];
-      "video/*"         = [ "totem.desktop"            ];
+      "application/pdf" = "org.gnome.Papers.desktop";
+      "image/*"         = "org.gnome.Loupe.desktop" ;
+      "video/*"         = "totem.desktop"           ;
 
-      "text/html"              = [ "firefox.desktop" ];
-      "text/xml"               = [ "firefox.desktop" ];
-      "x-scheme-handler/http"  = [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "text/html"              = "firefox.desktop";
+      "text/xml"               = "firefox.desktop";
+      "x-scheme-handler/http"  = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
     };
-    
   };
 };
 
