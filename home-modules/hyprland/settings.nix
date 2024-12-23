a406c2a629366760436e5c85589de5f0e91eadd1
ArtemChandragupta <artem020503@gmail.com>
2024-12-23 22:41:02 +0300

    regex games

--- home-modules/hyprland/settings.nix
+++ home-modules/hyprland/settings.nix
@@ -57,8 +57,9 @@ misc = {
 
   enable_swallow = true;
   swallow_regex  = "^(kitty)$";
+  # swallow_regex  = "^(kitty|ripdrag)$";
   # swallow_exception_regex = "^(it.catboy.ripdrag)$"; # Broken
-  swallow_exception_regex = "^ripdrag$";
+  swallow_exception_regex = "ripdrag";
 };
 
 general = {
