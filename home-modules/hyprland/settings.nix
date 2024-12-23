4bd0341d8d951dd741e81c2265abdf864e03068f
ArtemChandragupta <artem020503@gmail.com>
2024-12-23 22:59:22 +0300

    swllow exception is not what I think

--- home-modules/hyprland/settings.nix
+++ home-modules/hyprland/settings.nix
@@ -57,9 +57,6 @@ misc = {
 
   enable_swallow = true;
   swallow_regex  = "^(kitty)$";
-  # swallow_regex  = "^(kitty|ripdrag)$";
-  swallow_exception_regex = "it.catboy.ripdrag$"; # Broken
-  # swallow_exception_regex = "ripdrag";
 };
 
 general = {
