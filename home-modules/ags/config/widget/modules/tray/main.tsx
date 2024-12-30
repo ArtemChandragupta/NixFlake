c4c23046ced6281629356a3e6864f5f2dab46006
ArtemChandragupta <artem020503@gmail.com>
2024-12-31 01:04:52 +0300

    ags tray

---
+++ home-modules/ags/config/widget/modules/tray/main.tsx
@@ -0,0 +1,18 @@
+import { bind } from "astal"
+import Tray     from "gi://AstalTray"
+
+export function SysTray() {
+  const tray = Tray.get_default()
+
+  return <box className="SysTray">
+    {bind(tray, "items").as(items => items.map(item => (
+      <menubutton
+        tooltipMarkup = {bind(item, "tooltipMarkup")}
+        usePopover    = {false}
+        actionGroup   = {bind(item, "action-group").as(ag => ["dbusmenu", ag])}
+        menuModel     = {bind(item, "menu-model")}>
+        <icon gicon={bind(item, "gicon")} />
+      </menubutton>
+    )))}
+  </box>
+}
