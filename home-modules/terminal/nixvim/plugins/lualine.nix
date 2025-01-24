4d7b026ea0610cbc8b326eb5569fa0ea686d32d5
ArtemChandragupta <artem020503@gmail.com>
2025-01-24 15:08:52 +0300

    lualine new style

--- home-modules/terminal/nixvim/plugins/lualine.nix
+++ home-modules/terminal/nixvim/plugins/lualine.nix
@@ -2,13 +2,32 @@
 
 programs.nixvim.plugins.lualine = { # Statusline
   enable = true;
-  settings.options.icons_enabled = false;
+  settings = {
+    sections = {
+      lualine_a = ["mode"];
+      lualine_b = ["branch" "diff" "diagnostics"];
+      lualine_c = ["filename"];
+      lualine_x = ["filetype"];
+      lualine_y = ["progress"];
+      lualine_z = ["location"];
+    };
+    options = {
+      section_separators = {
+        left  = "";
+        right = "";
+      };
+      component_separators = {
+        left  = "";
+        right = "";
+      };
+    };
+  };
 };
 
 # For transparent parts of statusline
-programs.nixvim.extraConfigVim = ''
-  hi! link StatusLine Normal
-  hi! link StatusLineNC Normal
-'';
+# programs.nixvim.extraConfigVim = ''
+#   hi! link StatusLine Normal
+#   hi! link StatusLineNC Normal
+# '';
 
 }
