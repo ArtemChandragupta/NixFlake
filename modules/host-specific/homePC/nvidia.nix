dccf503516d6ae4d7b46e277b45143e84209c0a7
ArtemChandragupta <artem020503@gmail.com>
2025-01-26 15:30:06 +0300

    nvidia driver downgrade

--- modules/host-specific/homePC/nvidia.nix
+++ modules/host-specific/homePC/nvidia.nix
@@ -12,7 +12,7 @@ hardware.nvidia = {
   };
   open = false;
   nvidiaSettings = true;
-  package = config.boot.kernelPackages.nvidiaPackages.beta;
+  # package = config.boot.kernelPackages.nvidiaPackages.beta;
 };
 
 }
