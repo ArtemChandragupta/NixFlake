8654cafcfd6149941b864e84741b3cc68692b962
ArtemChandragupta <artem020503@gmail.com>
2025-02-18 12:48:00 +0300

    more icons

--- home-modules/hyprland/waybar/icons.nix
+++ home-modules/hyprland/waybar/icons.nix
@@ -10,22 +10,22 @@ programs.waybar.settings.mainBar."hyprland/workspaces" = {
     "class<firefox>"  = "󰈹";
     "class<discord>"  = "";
     "class<kitty>"    = "";
+    "class<mpv>"      = "󰃽";
+    "class<neovide>"  = "";
 
-    "class<org.gnome.Software>"   = "󱗼";
-    "class<org.gnome.Nautilus>"   = "󰪶";
-    "class<org.gnome.Papers>"     = "";
-    "class<org.gnome.Loupe>"      = "";
+    "class<org.gnome.Software>" = "󱗼";
+    "class<org.gnome.Nautilus>" = "󰪶";
+    "class<org.gnome.Papers>"   = "";
+    "class<org.gnome.Loupe>"    = "";
 
     "class<com.moonlight_stream.Moonlight>"= "󰢹";
     "class<com.github.rafostar.Clapper>"   = "󱉺";
-    "class<ONLYOFFICE Desktop Editors>"    = "";
     "class<Mendeley Reference Manager>" = "";
     "class<de.haeckerfelix.Fragments>"  = "";
     "class<io.github.nokse22.Exhibit>"  = "󰐫";
     "class<com.saivert.pwvucontrol>"    = "󰴸";
     "class<com.usebottles.bottles>" = "";
     "class<page.kramo.Cartridges>"  = "";
-    "class<org.cvfosammmm.Setzer>"  = "󱗆";
     "class<org.telegram.desktop>"   = "";
     "class<libreoffice-writer>"     = "";
     "class<app.drey.Dialect>" = "󱁥";
@@ -34,9 +34,7 @@ programs.waybar.settings.mainBar."hyprland/workspaces" = {
     "class<dosbox-staging>"   = "󰚴";
     "class<localsend_app>"    = "󱗿";
     "class<virt-manager>" = "";
-    "class<calculator>"   = "󰇃";
-    "class<translator>"   = "󱁥";
-    "class<Texmaker>" = "";
+    "class<calc>"     = "󰇃";
     "class<openscad>" = "";
   };
 
