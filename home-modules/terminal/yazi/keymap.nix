bd95ebb2b29295a82ccfe8f868c140d092ebc301
ArtemChandragupta <artem020503@gmail.com>
2025-02-25 15:31:47 +0300

    yazi: esc from shell window, nix: no dirty git tree

--- home-modules/terminal/yazi/keymap.nix
+++ home-modules/terminal/yazi/keymap.nix
@@ -1,31 +1,35 @@
 {
 
-programs.yazi.keymap.manager.prepend_keymap = [
-  {
-    on   = "<Enter>";
-    run  = "plugin smart-enter";
-    desc = "Enter directory or open file";
-  }
-  {
-    on   = "<Esc>";
-    run  = "close";
-    desc = "Cancel input. Not working";
-  }
-  { # I escape to bash from nushell to get background tasks
-    on   = "<C-n>";
-    run  = ''shell 'bash -c "ripdrag -xnis 350 \"$1\" &"' --confirm'';
-    desk = "Drag-and-drop";
-  }
-  {
-    on   = "<C-S-y>";
-    run  = ["shell 'echo \"$@\" | wl-copy'" "toggle_all --state=off"];
-    desk = "Copy selected files path to system clipboard";
-  }
-  {
-    on   = "<C-S-p>";
-    run  = "shell 'paste' ";
-    desk = "Paste files from path in system clipboard";
-  }
-];
+programs.yazi.keymap = {
+    manager.prepend_keymap = [
+    {
+      on   = "<Enter>";
+      run  = "plugin smart-enter";
+      desc = "Enter directory or open file";
+    }
+    { # I escape to bash from nushell to get background tasks
+      on   = "<C-n>";
+      run  = ''shell 'bash -c "ripdrag -xnis 350 \"$1\" &"' --confirm'';
+      desk = "Drag-and-drop";
+    }
+    {
+      on   = "<C-S-y>";
+      run  = ["shell 'echo \"$@\" | wl-copy'" "toggle_all --state=off"];
+      desk = "Copy selected files path to system clipboard";
+    }
+    {
+      on   = "<C-S-p>";
+      run  = "shell 'paste' ";
+      desk = "Paste files from path in system clipboard";
+    }
+  ];
+  input.prepend_keymap = [
+    {
+      on   = "<Esc>";
+      run  = "close";
+      desc = "Cancel input";
+    }
+  ];
+};
 
 }
--- modules/system/system.nix
+++ modules/system/system.nix
@@ -4,6 +4,7 @@ nix = {
   settings = { 
     auto-optimise-store = true;
     experimental-features = "nix-command flakes";
+    warn-dirty = false;
   };
   gc = {
     automatic = true;
