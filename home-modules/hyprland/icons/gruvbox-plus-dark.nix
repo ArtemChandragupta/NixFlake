ff36c639983553c70e3c7684773375985b44b5e4
ArtemChandragupta <artem020503@gmail.com>
2025-06-13 23:26:42 +0300

    icon update

--- home-modules/hyprland/icons/gruvbox-plus-dark.nix
+++ home-modules/hyprland/icons/gruvbox-plus-dark.nix
@@ -8,17 +8,15 @@ stdenvNoCC.mkDerivation {
   src = fetchFromGitHub {
     owner = "SylEleuth";
     repo = "gruvbox-plus-icon-pack";
-    rev = "8480fd222392e04f21ed5659a0b322a486221739";
-    hash = "sha256-eqbG8BSU2sC7EuuJHAJ8eLRiafxKhbdiY5ipg/cJ4jU=";
+    rev = "5acbc2fd175bfedcafe37740fcc9581a92ea6588";
+    hash = "sha256-hIbWjg8QsH6nqD3T6US6e2WAgmofjzCrejwWVvZywoA=";
   };
 
   # src = fetchFromGitHub {
   #   owner = "artemchandragupta";
   #   repo = "gruvbox-plus-icon-pack";
-  #   rev = "b24c4e6d101a0099ba5bca4340e7717752101134";
-  # hash = "sha256-RiBOehJxWnrlyFxR5JD1gSxeJJJD+Td1KbVHLSjsRmk=";
-  #   # rev = "e2b9a8fdeadc231dec0f9a733c3770356abf94a8";
-  # # hash = "sha256-CH2PhTdwBAC2YjDMbyX0Yu/ggq/qWjGhceujUZ8uy+I=";
+  #   rev = "e2b9a8fdeadc231dec0f9a733c3770356abf94a8";
+  #   hash = "sha256-CH2PhTdwBAC2YjDMbyX0Yu/ggq/qWjGhceujUZ8uy+I=";
   # };
 
   dontBuild = true;
