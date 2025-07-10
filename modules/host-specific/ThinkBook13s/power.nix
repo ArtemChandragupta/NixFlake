9cb75de292bf75c0426fac5a42eb69339c7a8442
ArtemChandragupta <artem020503@gmail.com>
2025-07-10 23:16:18 +0300

    remove autocpufreq mention

--- modules/host-specific/ThinkBook13s/power.nix
+++ modules/host-specific/ThinkBook13s/power.nix
@@ -1,24 +1,7 @@
 {
 
 powerManagement.enable = true;
-
-services.auto-cpufreq = { 
-  # enable = true;
-  settings = {
-    battery =  {
-      governor = "powersave";
-      turbo    = "never";
-    };
-    charger = {
-      governor = "powersave";
-      turbo    = "auto";
-    };  
-  };
-};
-services.upower.enable = true; # For battery service
-services.power-profiles-daemon = {
-  enable = true;
-};
-
+services.upower.enable = true;
+services.power-profiles-daemon.enable = true;
 
 }
