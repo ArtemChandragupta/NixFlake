f327e243735505d301e25e6ed55111044a35d4ad
ArtemChandragupta <artem020503@gmail.com>
2025-08-08 20:51:03 +0300

    gtkgreetd name nixpkgs update

--- modules/wm/dm.nix
+++ modules/wm/dm.nix
@@ -4,7 +4,7 @@ services.greetd = {
   enable = true;
   settings = {
     default_session = {
-      command = "${pkgs.cage}/bin/cage -s -d -- ${pkgs.greetd.gtkgreet}/bin/gtkgreet -c Hyprland &> /tmp/hypland-start.log";
+      command = "${pkgs.cage}/bin/cage -s -d -- ${pkgs.gtkgreet}/bin/gtkgreet -c Hyprland &> /tmp/hypland-start.log";
       user    = "greeter";
     };
     initial_session = {
