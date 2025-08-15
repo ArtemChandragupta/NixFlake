367486dfcdb5c72b38e4a6ddb8151b671db0b670
ArtemChandragupta <artem020503@gmail.com>
2025-08-15 04:10:24 +0300

    firefox clearup

--- home-modules/apps/firefox/solid/default.nix
+++ home-modules/apps/firefox/solid/default.nix
@@ -3,7 +3,7 @@
 programs.firefox.profiles.default = {
   userChrome  = builtins.readFile ./userChrome.css;
   settings = {
-    "browser.tabs.allow_transparent_browser" = false; # For transparent frame
+    "browser.tabs.allow_transparent_browser" = false;
     "startup.homepage_welcome_url" = "https://github.com/ArtemChandragupta/NixFlake/tree/main/home-modules/firefox/solid";
   };
 };
