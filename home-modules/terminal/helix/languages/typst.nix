bccbd372708cb519330800e5483dda4af9f19ee8
ArtemChandragupta <artem020503@gmail.com>
2025-08-16 04:34:47 +0300

    ltex-plus lsp for typos!

--- home-modules/terminal/helix/languages/typst.nix
+++ home-modules/terminal/helix/languages/typst.nix
@@ -1,17 +1,23 @@
 { pkgs, ... }:{
 
 programs.helix = {
-  extraPackages = [ pkgs.tinymist ];  
+  extraPackages = [ pkgs.tinymist pkgs.ltex-ls-plus];
   languages = {
     
     language = [{
       name = "typst";
       soft-wrap.enable = true;
+      language-servers = ["tinymist" "ltex"];
     }];
     
-    language-server.tinymist.config = {
-      typstExtraArgs = [ "main.typ" ]; # maybe I need set this in typst projects
-      exportPdf = "onType";
+    language-server = {
+      tinymist.config = {
+        typstExtraArgs = [ "main.typ" ]; # maybe I need set this in typst projects
+        exportPdf = "onType";
+      };
+      ltex = {
+        command = "ltex-ls-plus";
+      };
     };
 
   };
