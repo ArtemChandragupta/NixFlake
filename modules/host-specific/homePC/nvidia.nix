4f6d6ab1e23d281d77a101549af7aa9cee0628e7
ArtemChandragupta <artem020503@gmail.com>
2025-08-17 22:13:44 +0300

    nvidia module simplified

--- modules/host-specific/homePC/nvidia.nix
+++ modules/host-specific/homePC/nvidia.nix
@@ -1,18 +1,12 @@
-{ config, ...}:{
-
-boot.kernelParams = ["nvidia.NVreg_PreserveVideoMemoryAllocations=1"];
+{
 
 services.xserver.videoDrivers = ["nvidia"];
 
 hardware.nvidia = {
   modesetting.enable = true;
-  powerManagement = {
-    enable = false;
-    finegrained = false;
-  };
+  powerManagement.enable = true;
   open = true;
   nvidiaSettings = true;
-  package = config.boot.kernelPackages.nvidiaPackages.latest;
 };
 
 }
