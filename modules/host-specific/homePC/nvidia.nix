4a80df629a32e3829176299167527fd29245f133
ArtemChandragupta <artem020503@gmail.com>
2025-08-25 02:51:06 +0300

    nvidia driver version fix

--- modules/host-specific/homePC/nvidia.nix
+++ modules/host-specific/homePC/nvidia.nix
@@ -1,8 +1,16 @@
-{
+{ config, ... }:{
 
 services.xserver.videoDrivers = ["nvidia"];
 
 hardware.nvidia = {
+  package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "575.64.05";
+    sha256_64bit = "sha256-hfK1D5EiYcGRegss9+H5dDr/0Aj9wPIJ9NVWP3dNUC0=";
+    sha256_aarch64 = "sha256-GRE9VEEosbY7TL4HPFoyo0Ac5jgBHsZg9sBKJ4BLhsA=";
+    openSha256 = "sha256-mcbMVEyRxNyRrohgwWNylu45vIqF+flKHnmt47R//KU=";
+    settingsSha256 = "sha256-o2zUnYFUQjHOcCrB0w/4L6xI1hVUXLAWgG2Y26BowBE=";
+    persistencedSha256 = "sha256-2g5z7Pu8u2EiAh5givP5Q1Y4zk4Cbb06W37rf768NFU=";
+  };
   modesetting.enable = true;
   powerManagement.enable = true;
   open = true;
