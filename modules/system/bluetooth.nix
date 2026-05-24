916d74621b0f10d76370fa146208b0536c56ecf6
ArtemChandragupta <artem020503@gmail.com>
2026-05-24 04:48:31 +0300

    bluetooth fix

--- modules/system/bluetooth.nix
+++ modules/system/bluetooth.nix
@@ -1,9 +1,7 @@
-{
+{ var, ... }: {
 
 hardware.bluetooth.enable = true;
-services.blueman = {
-  enable     = true;
-  withApplet = true;
-};
+services.blueman.enable = true;
+home-manager.users.${var.user}.services.blueman-applet.enable = true;
 
 }
